adad1efce3a56e58a0471fbd61a43de9d698f4aa
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-11-02 23:40:01 +0900

    Update scroll view

--- app/src/main/java/com/example/samplescroolview/MainActivity.kt
+++ app/src/main/java/com/example/samplescroolview/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.samplescroolview.ui.theme.SampleScroolViewTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,8 +43,28 @@ fun ScrollView() {
         modifier = Modifier
             .verticalScroll(rememberScrollState())
     ) {
-        repeat(100) {
-            Text("aaaaaaaa")
+        // Todo: ここをいい感じに書ければよさそう
+        ArticleList()
+        Spacer(modifier = Modifier.padding(30.dp))
+        ArticleList()
+        Spacer(modifier = Modifier.padding(30.dp))
+        ArticleList()
+        Spacer(modifier = Modifier.padding(30.dp))
+        ArticleList()
+        Spacer(modifier = Modifier.padding(30.dp))
+        ArticleList()
+        Spacer(modifier = Modifier.padding(30.dp))
+        ArticleList()
+    }
+}
+
+@Composable
+fun ArticleList() {
+    Column {
+        listOf("a", "b", "c", "d", "e").forEach {
+            Text(text = "ArticleList: $it")
+            Spacer(modifier = Modifier.padding(10.dp))
+            Divider()
         }
     }
 }
